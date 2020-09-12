ee1805782826e4c10ed787d8b7db33cfcac54b15
Gustavo Bicalho <gusbicalho@gmail.com>
2020-09-12 03:14:34 +0000

    implement BuildSpec

--- src/RowComponents.hs
+++ src/RowComponents.hs
@@ -1,7 +1,99 @@
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE PolyKinds #-}
+
 module RowComponents
-    ( projectName,
-    )
+  ( noDeps,
+    WithDeps (..),
+    withDeps,
+    withDep,
+  )
 where
 
-projectName :: String
-projectName = "row-components"
+import Data.Kind (Type)
+import Data.Row.Internal (LT ((:->)), Row (R))
+import Data.Row.Records (Empty, Rec, type (.+), type (.==))
+import GHC.TypeLits (Symbol)
+
+-- type family Matches (spec :: Row Type) (r :: Row Type) :: Constraint where
+--   Matches Empty r = ()
+--   Matches ('R ((label ':-> t) ': pairs)) r = ((r .! label) ~ t, Matches ('R pairs) r)
+
+-- class ((row .! label) ~ (Rec item), Matches spec item) => HasField row label spec item
+
+-- instance ((row .! label) ~ (Rec item), Matches spec item) => HasField row label spec item
+
+data BuildSpec (providedDeps :: Row Symbol) (requiredDeps :: Row Type) (a :: Type) where
+  NoDeps :: (Rec requiredDeps -> IO a) -> BuildSpec Empty requiredDeps a
+  ProvidingDep ::
+    forall fromName asName provided required a.
+    BuildSpec provided required a ->
+    BuildSpec (provided .+ fromName .== asName) required a
+
+{-# INLINE noDeps #-}
+noDeps :: IO a -> BuildSpec Empty Empty a
+noDeps build = NoDeps (const build)
+
+type family ZipToRow (labels :: [Symbol]) (values :: [k]) :: Row k where
+  ZipToRow '[] _ = Empty
+  ZipToRow _ '[] = Empty
+  ZipToRow (l ': ls) (v ': vs) = (l .== v) .+ ZipToRow ls vs
+
+class WithDeps (deps :: Row Symbol) (spec :: Type) (spec' :: Type) | spec deps -> spec' where
+  withRenamedDeps :: spec -> spec'
+
+withDeps ::
+  forall (deps :: [Symbol]) spec spec'.
+  WithDeps (ZipToRow deps deps) spec spec' =>
+  spec ->
+  spec'
+{-# INLINE withDeps #-}
+withDeps = withRenamedDeps @(ZipToRow deps deps)
+
+withDep ::
+  forall (dep :: Symbol) spec spec'.
+  WithDeps (dep .== dep) spec spec' =>
+  spec ->
+  spec'
+{-# INLINE withDep #-}
+withDep = withRenamedDeps @(dep .== dep)
+
+instance
+  ( WithDeps
+      deps
+      (BuildSpec Empty requiredDeps a)
+      (BuildSpec providedDeps requiredDeps a)
+  ) =>
+  WithDeps
+    deps
+    (Rec requiredDeps -> IO a)
+    (BuildSpec providedDeps requiredDeps a)
+  where
+  {-# INLINE withRenamedDeps #-}
+  withRenamedDeps buildFn = (NoDeps buildFn) & withRenamedDeps @deps
+
+instance
+  (providedDeps ~ providedDeps') =>
+  WithDeps
+    Empty
+    (BuildSpec providedDeps requiredDeps a)
+    (BuildSpec providedDeps' requiredDeps a)
+  where
+  {-# INLINE withRenamedDeps #-}
+  withRenamedDeps buildSpec = buildSpec
+
+instance
+  ( WithDeps
+      ('R pairs)
+      (BuildSpec (providedDeps .+ l .== v) requiredDeps a)
+      (BuildSpec providedDeps' requiredDeps a)
+  ) =>
+  WithDeps
+    ('R ((l ':-> v) ': pairs))
+    (BuildSpec providedDeps requiredDeps a)
+    (BuildSpec providedDeps' requiredDeps a)
+  where
+  {-# INLINE withRenamedDeps #-}
+  withRenamedDeps buildFn = (ProvidingDep @l @v buildFn) & withRenamedDeps @('R pairs)
--- test/RowComponentsSpec.hs
+++ test/RowComponentsSpec.hs
@@ -1,10 +1,96 @@
+{-# LANGUAGE PartialTypeSignatures #-}
+{-# OPTIONS_GHC -Wno-partial-type-signatures #-}
+
 module RowComponentsSpec
-    ( spec,
-    )
+  ( spec,
+  )
 where
 
-import RowComponents (projectName)
+import Data.Row.Records
+  ( (.!),
+    (.+),
+    (.==),
+    Rec,
+    type (.+),
+    type (.==),
+  )
+import RowComponents (noDeps, withDep, withDeps, withRenamedDeps)
+import System.Random (randomRIO)
 import Test.Hspec (SpecWith, it, shouldBe)
 
 spec :: SpecWith ()
-spec = it "passes" $ projectName `shouldBe` "row-components"
+spec = it "passes" $ system `seq` (1 :: Int) `shouldBe` 1
+
+system :: _
+system =
+  #foo .== (makeFoo & noDeps)
+    .+ #bar .== (makeBar & withDep @"foo")
+    .+ #qwe
+      .== ( makeQwe
+              & withDep @"foo"
+              & withDep @"bar"
+          )
+    .+ #asd .== (makeAsd & withDeps @'["foo", "bar", "qwe"])
+    .+ #app .== (makeApp & withRenamedDeps @("first" .== "foo" .+ "second" .== "asd"))
+
+type IFoo f = "getFoo" .== f Int
+
+type Foo = Rec (IFoo IO)
+
+makeFoo :: IO Foo
+makeFoo = pure $ #getFoo .== getFoo
+  where
+    getFoo = randomRIO (1, 100)
+
+type IBar f = "getBarS" .== f String .+ "getBarI" .== f Int
+
+type Bar = Rec (IBar IO)
+
+makeBar :: Rec ("foo" .== Foo) -> IO Bar
+makeBar deps =
+  pure $
+    #getBarI .== getBarI
+      .+ #getBarS .== pure "bar"
+  where
+    getBarI = ((3 *) <$> deps .! #foo .! #getFoo)
+
+type IQwe f = "getQwe" .== f Int
+
+type Qwe = Rec (IQwe IO)
+
+makeQwe ::
+  Rec ("foo" .== Foo .+ "bar" .== Bar) -> IO Qwe
+makeQwe deps = pure $ #getQwe .== getQwe
+  where
+    getQwe = (*) <$> deps .! #foo .! #getFoo <*> deps .! #bar .! #getBarI
+
+-- makeQwe deps =
+
+type IAsd f = "getAsd" .== f String
+
+type Asd = Rec (IAsd IO)
+
+makeAsd ::
+  Rec ("foo" .== Foo .+ "bar" .== Bar .+ "qwe" .== Qwe) -> IO Asd
+makeAsd deps = pure $ #getAsd .== getAsd
+  where
+    getAsd = do
+      foo <- deps .! #foo .! #getFoo
+      barI <- deps .! #bar .! #getBarI
+      barS <- deps .! #bar .! #getBarS
+      qwe <- deps .! #qwe .! #getQwe
+      pure $ show foo <> show barI <> barS <> show qwe
+
+type IApp f = "getApp" .== f [String]
+
+type App = Rec (IApp IO)
+
+makeApp ::
+  Rec ("first" .== Foo .+ "second" .== Asd) -> IO App
+makeApp deps = pure $ #getApp .== getApp
+  where
+    getApp =
+      sequence
+        [ show <$> deps .! #first .! #getFoo,
+          deps .! #second .! #getAsd
+        ]
